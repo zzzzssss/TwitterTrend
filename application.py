46e70aa6ddfd93c5f2f4d7f9564af50f2ce838d6
blairmn <shuyang@ZhaoShuyangs-MacBook-Pro.local>
2017-04-08 01:07:34 -0400

    test

--- application.py
+++ application.py
@@ -19,15 +19,17 @@ socketio = SocketIO(application)
 #socketConnected = False
 
 
-host='search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com'
-esurl='http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com/twittertrend/tweets'
-es = Elasticsearch(
-    hosts=[{'host': host, 'port': 443}],
-    use_ssl=True,
-    verify_certs=True,
-    #connection_class=RequestsHttpConnection
-) 
-print(es.info())
+#host='search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com'
+#esurl='http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com/twittertrend/tweets'
+host='http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com'
+client = Elasticsearch([host])
+# es = Elasticsearch(
+#     hosts=[{'host': host, 'port': 443}],
+#     use_ssl=True,
+#     verify_certs=True,
+#     #connection_class=RequestsHttpConnection
+# ) 
+# print(es.info())
 
 
 
@@ -110,7 +112,8 @@ def home():
         if hdr == 'Notification':
             tweet = js['Message']
             print tweet
-            es.index(index="twittertrend", doc_type="tweets", id=js['MessageId'], body= tweet)
+            client.index(index="twittertrend", doc_type="tweets", id=js['MessageId'], body= tweet)
+            #es.index(index="twittertrend", doc_type="tweets", id=js['MessageId'], body= tweet)
             #r = requests.post(esurl, json = tweet)
         if socketConnected:
                 socketio.emit('realTimeResponse', tweet)
