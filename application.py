9382761c62e9a22935e6cad079e90e7caa80ee89
blairmn <shuyang@dyn-160-39-135-50.dyn.columbia.edu>
2017-04-08 15:04:39 -0400

    test

--- application.py
+++ application.py
@@ -135,7 +135,7 @@ def home():
             #client.index(index="twittertrend", doc_type="tweets", id=js['MessageId'], body= tweet)
             #es.index(index="twittertrend", doc_type="tweets", id=js['MessageId'], body= tweet)
             #r = requests.post(esurl, json=tweet)
-            r = requests.post(esurl, data=json.dumps(tweet))
+            r = requests.post(esurl, data=tweet)
 
             print r.text
         # if socketConnected:
