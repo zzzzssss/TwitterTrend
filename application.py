8d116fc2ade857ae42353a3d5d9b0fee62f423a4
blairmn <shuyang@ZhaoShuyangs-MacBook-Pro.local>
2017-04-08 12:05:53 -0400

    test

--- application.py
+++ application.py
@@ -118,7 +118,7 @@ def home():
             print type(tweet)
             #client.index(index="twittertrend", doc_type="tweets", id=js['MessageId'], body= tweet)
             #es.index(index="twittertrend", doc_type="tweets", id=js['MessageId'], body= tweet)
-            r = requests.post(esurl, data=json.dumps(tweet, ensure_ascii=True))
+            r = requests.post(esurl, tweets=json.dumps(tweet, ensure_ascii=True))
             print r
         # if socketConnected:
         #         socketio.emit('realTimeResponse', tweet)
