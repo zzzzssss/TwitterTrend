226ea94ef32e167432834f076666b7577b794310
blairmn <shuyang@dyn-160-39-135-50.dyn.columbia.edu>
2017-04-08 16:37:07 -0400

    test

--- application.py
+++ application.py
@@ -21,73 +21,77 @@ esurl='http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com
 #     connection_class=RequestsHttpConnection
 # ) 
 # print(es.info())
+@application.route('/home', methods=['GET','POST'])
+def home():
+
+return render_template('home1.html', marker_list = [], count='')
 
 
 
-# @application.route('/', methods=['POST'])
-# def map():    
-# creating a map in the view
-    # try:
-    #     dp_res = request.form['dropdown']
-    #     dp_res2=request.form['dropdown2']
-    # except:
-    #     return render_template('home1.html', marker_list = [], count='')
+@application.route('/home', methods=['POST'])
+def map():    
+creating a map in the view
+    try:
+        dp_res = request.form['dropdown']
+        dp_res2=request.form['dropdown2']
+    except:
+        return render_template('home1.html', marker_list = [], count='')
     
 
-    # selected = dp_res
-    # maxsize=int(dp_res2)
-    # print type(selected), dp_res2, maxsize
+    selected = dp_res
+    maxsize=int(dp_res2)
+    print type(selected), dp_res2, maxsize
         
-    # res = es.search(index="twittertrend", doc_type="tweets", q=selected, size=maxsize)
-    # locationst=[]
+    res = es.search(index="twittertrend", doc_type="tweets", q=selected, size=maxsize)
+    locationst=[]
 
-    # print("%d documents found" % res['hits']['total'])
+    print("%d documents found" % res['hits']['total'])
 
 
-    # print len(res['hits']['hits'])
-    # for doc in res['hits']['hits']:
-    #     #print doc
-    #     #print("%s) %s" % (doc['_id'], doc['_source']['text']))
-    #     #print doc['_source']['coordinates']
-    #     text=doc['_source']['text']
-    #     if doc['_source']['coordinates']:
-    #         x= doc['_source']['coordinates']['coordinates']
-    #         locationst.append([x, text])
+    print len(res['hits']['hits'])
+    for doc in res['hits']['hits']:
+        #print doc
+        #print("%s) %s" % (doc['_id'], doc['_source']['text']))
+        #print doc['_source']['coordinates']
+        text=doc['_source']['text']
+        if doc['_source']['coordinates']:
+            x= doc['_source']['coordinates']['coordinates']
+            locationst.append([x, text])
 
-    #     # select a random coordinates    
-    #     else:
+        # select a random coordinates    
+        else:
             
-    #         radius = 2113000.0                       #Choose your own radius
-    #         radiusInDegrees=float(radius/111300)            
-    #         r = radiusInDegrees
+            radius = 2113000.0                       #Choose your own radius
+            radiusInDegrees=float(radius/111300)            
+            r = radiusInDegrees
             
-    #         #   UScenter = {lat: 40.461881, lng: -99.757229};
-    #         x0 = 40.84
-    #         y0 = -99.757229
+            #   UScenter = {lat: 40.461881, lng: -99.757229};
+            x0 = 40.84
+            y0 = -99.757229
          
-    #         u = float(random.uniform(0.0,1.0))
-    #         v = float(random.uniform(0.0,1.0))
+            u = float(random.uniform(0.0,1.0))
+            v = float(random.uniform(0.0,1.0))
           
-    #         w = r * math.sqrt(u)
-    #         t = 2 * math.pi * v
+            w = r * math.sqrt(u)
+            t = 2 * math.pi * v
         
-    #         x = w * math.cos(t) 
-    #         y = w * math.sin(t)
+            x = w * math.cos(t) 
+            y = w * math.sin(t)
 
-    #         xLat  = x + x0
-    #         yLong = y + y0
-    #         point = (xLat, yLong)
-    #         locationst.append([point, text])
+            xLat  = x + x0
+            yLong = y + y0
+            point = (xLat, yLong)
+            locationst.append([point, text])
 
-    # # number of tweets   
-    # number = len(locationst)  
-    # #print locationst[1][0]
-    # #print locationst[1][1]
-    # return render_template('home1.html', marker_list= locationst, count=number, selected=selected)
+    # number of tweets   
+    number = len(locationst)  
+    #print locationst[1][0]
+    #print locationst[1][1]
+    return render_template('home1.html', marker_list= locationst, count=number, selected=selected)
 
 
 @application.route('/', methods=['GET','POST'])
-def home():
+def sns():
     global socketConnected
 
     if request.method == 'POST':
@@ -108,7 +112,8 @@ def home():
                 socketio.emit('realTimeResponse', tweet_js)
 
 
-    return render_template('home1.html', marker_list = [], count='')
+    #return render_template('home1.html', marker_list = [], count='')
+
 
 @socketio.on('realTime')
 def handle_my_custom_event(message):
