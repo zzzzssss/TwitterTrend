e576042ef2d0413227d71e077d07dff49399d3e8
blairmn <shuyang@dyn-160-39-135-50.dyn.columbia.edu>
2017-04-08 22:47:45 -0400

    test

--- application.py
+++ application.py
@@ -29,80 +29,80 @@ awsauth=AWS4Auth('AKIAJ4DLBU4HQGRC37FA', 'py3eHrRr1TQ0PzADrCzdAQsHtKtCnS0TcmA/lq
 
 
 
-@application.route('/home', methods=['POST'])
-def map():    
-#creating a map in the view
-    try:
-        dp_res = request.form['dropdown']
-        dp_res2=request.form['dropdown2']
-    except:
-        return render_template('home1.html', marker_list = [], count='')
+# @application.route('/home', methods=['POST'])
+# def map():    
+# #creating a map in the view
+#     try:
+#         dp_res = request.form['dropdown']
+#         dp_res2=request.form['dropdown2']
+#     except:
+#         return render_template('home1.html', marker_list = [], count='')
     
 
-    selected = dp_res
-    maxsize=int(dp_res2)
-    #print type(selected), dp_res2, maxsize
+#     selected = dp_res
+#     maxsize=int(dp_res2)
+#     #print type(selected), dp_res2, maxsize
 
-    # queryURL = 'http://localhost:9201/tweetmap/_search?q=*:*&size=1000'
-    queryURL = 'http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com/test-index2/tweet/_search?q='
-    queryURL=queryURL+selected+'&size='+str(maxsize)
-    print queryURL
-    response = requests.get(queryURL)
-    res = json.loads(response.text)
-    #print res['hits']['hits']
+#     # queryURL = 'http://localhost:9201/tweetmap/_search?q=*:*&size=1000'
+#     queryURL = 'http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com/test-index2/tweet/_search?q='
+#     queryURL=queryURL+selected+'&size='+str(maxsize)
+#     print queryURL
+#     response = requests.get(queryURL)
+#     res = json.loads(response.text)
+#     #print res['hits']['hits']
         
-    #res = es.search(index="twittertrend", doc_type="tweets", q=selected, size=maxsize)
-    locationst=[]
+#     #res = es.search(index="twittertrend", doc_type="tweets", q=selected, size=maxsize)
+#     locationst=[]
 
-    print("%d documents found" % res['hits']['total'])
+#     print("%d documents found" % res['hits']['total'])
 
 
-    print len(res['hits']['hits'])
-    for doc in res['hits']['hits']:
-        #print doc
-        #print("%s) %s" % (doc['_id'], doc['_source']['text']))
-        #print doc['_source']['coordinates']
-        text=doc['_source']['text']
-        if doc['_source']['location']:
-            x= doc['_source']['location']
-            locationst.append([x, text])
+#     print len(res['hits']['hits'])
+#     for doc in res['hits']['hits']:
+#         #print doc
+#         #print("%s) %s" % (doc['_id'], doc['_source']['text']))
+#         #print doc['_source']['coordinates']
+#         text=doc['_source']['text']
+#         if doc['_source']['location']:
+#             x= doc['_source']['location']
+#             locationst.append([x, text])
 
-        # select a random coordinates    
-        else:
+#         # select a random coordinates    
+#         else:
             
-            radius = 2113000.0                       #Choose your own radius
-            radiusInDegrees=float(radius/111300)            
-            r = radiusInDegrees
+#             radius = 2113000.0                       #Choose your own radius
+#             radiusInDegrees=float(radius/111300)            
+#             r = radiusInDegrees
             
-            #   UScenter = {lat: 40.461881, lng: -99.757229};
-            x0 = 40.84
-            y0 = -99.757229
+#             #   UScenter = {lat: 40.461881, lng: -99.757229};
+#             x0 = 40.84
+#             y0 = -99.757229
          
-            u = float(random.uniform(0.0,1.0))
-            v = float(random.uniform(0.0,1.0))
+#             u = float(random.uniform(0.0,1.0))
+#             v = float(random.uniform(0.0,1.0))
           
-            w = r * math.sqrt(u)
-            t = 2 * math.pi * v
+#             w = r * math.sqrt(u)
+#             t = 2 * math.pi * v
         
-            x = w * math.cos(t) 
-            y = w * math.sin(t)
+#             x = w * math.cos(t) 
+#             y = w * math.sin(t)
 
-            xLat  = x + x0
-            yLong = y + y0
-            point = (xLat, yLong)
-            locationst.append([point, text])
+#             xLat  = x + x0
+#             yLong = y + y0
+#             point = (xLat, yLong)
+#             locationst.append([point, text])
 
-    # number of tweets   
-    number = len(locationst)  
-    #print locationst[1][0]
-    #print locationst[1][1]
-    return render_template('home1.html', marker_list= locationst, count=number, selected=selected)
+#     # number of tweets   
+#     number = len(locationst)  
+#     #print locationst[1][0]
+#     #print locationst[1][1]
+#     return render_template('home1.html', marker_list= locationst, count=number, selected=selected)
     
 
 @application.route('/home', methods=['GET','POST'])
 def home():
 
-    return render_template('home1.html', marker_list = [], count='')
+    return render_template('home1.html')
 
 
 @application.route('/', methods=['GET','POST'])
@@ -137,16 +137,44 @@ def handle_my_custom_event(message):
     print('received message:' + message)
     # Fetch tweets in elastic search
     # queryURL = 'http://localhost:9201/tweetmap/_search?q=*:*&size=1000'
-    queryURL = 'http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com/test-index2/tweet/_search?q=*:*&size=100'
-    response = requests.get(queryURL)
-    results = json.loads(response.text)
+    # queryURL = 'http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com/test-index2/tweet/_search?q=*:*&size=100'
+    # response = requests.get(queryURL)
+    # results = json.loads(response.text)
 
+    # tweets = []
+    # for result in results['hits']['hits']:
+    #     tweet = {'sentiment': result['_source']['sentiment'], 'location': result['_source']['location']}
+    #     tweets.append(tweet)
+
+    # send(json.dumps(tweets))
+@socketio.on('message')
+def handle_message(message):
+    if message == 'Init':
+        # Run local elastic search
+        #queryURL = 'elastic search endpoint'
+        # queryURL = 'http://localhost:9201/tweetmap/_search?q=*:*&size=1000'
+        queryURL = 'http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com/test-index2/tweet/_search?q=*:*&size=100'
+        response = requests.get(queryURL)
+        results = json.loads(response.text)
+
+        print("INIT MAP")
+    else:
+        queryKeyWord = message.replace(' ', '%20')
+        queryURL = 'elastic search endpoint'
+        # queryURL = 'http://localhost:9201/tweetmap/_search?q=' + queryKeyWord + '&size=1000'
+        queryURL = 'http://search-movie-vpmtwgvr57yoata6seazfnpyfe.us-west-2.es.amazonaws.com/test-index2/tweet/_search?q='
+        queryURL=queryURL+selected+'&size=100'
+        response = requests.get(queryURL)
+        results = json.loads(response.text)
+        print("SEARCH" + str(message))
+
+    # Find locations of each tweet
     tweets = []
     for result in results['hits']['hits']:
         tweet = {'sentiment': result['_source']['sentiment'], 'location': result['_source']['location']}
         tweets.append(tweet)
 
-    #send(json.dumps(tweets))
+    send(json.dumps(tweets))
 
 
 if __name__ == '__main__':
